b1d28b7adb902be793da0acc1d55ea6e390dbbf0
arofy01 <arofywaliyy28@gmail.com>
2024-11-09 12:05:12 +0700

    debugging view

--- resources/views/beranda/siperbakin/home.blade.php
+++ resources/views/beranda/siperbakin/home.blade.php
@@ -131,6 +131,30 @@
                         <div class="carousel-inner">
                             <div class="carousel-item active">
                                 <div class="heading_box">
+                                  <nav class="nav">
+        <div class="container">
+            <div class="logo">
+                <a href="#"></a>
+            </div>
+            <div id="mainListDiv" class="main_list">
+                <ul class="navlinks">
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Tentang kami</a></li>
+                    <li><a href="#">layanan</a></li>
+									<li><button><a href="#">Login</a></button></li>
+                </ul>
+            </div>
+            <span class="navTrigger">
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+        </div>
+    </nav>
+
+    <section class="home">
+    </section>
+    <div style="height: 1000px">
                                     <h1>
                                         <span>
                                             Sistem
